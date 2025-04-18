85cf33a6d95c4191acb8a1a9a93516b24e3144c9
FrenchFry36 <saidakbarov@protonmail.com>
2025-04-18 16:48:01 +0200

    connected to server.js

--- web/src/mocks/handlers.js
+++ web/src/mocks/handlers.js
@@ -1,37 +1,6 @@
 import { http, HttpResponse } from 'msw';
 import { v4 as uuidv4 } from 'uuid';
-
-const allTasks = new Map();
-
-allTasks
-  .set(uuidv4(), {
-    title: 'Re-work UI/UX',
-    priority: 'Low',
-    releaseDate: '12/05/2025',
-    assignedTo: 'Said, Rachael',
-    projectName: 'Time App',
-  })
-  .set(uuidv4(), {
-    title: 'Dark mode toggle',
-    priority: 'High',
-    releaseDate: '09/03/2025',
-    assignedTo: 'Umair, Precious',
-    projectName: 'ASA Darkmode Feature',
-  })
-  .set(uuidv4(), {
-    title: 'Accessibility checks',
-    priority: 'Medium',
-    releaseDate: '15/04/2025',
-    assignedTo: 'Michael, Ricardo',
-    projectName: 'Time App',
-  })
-  .set(uuidv4(), {
-    title: 'Notification integration',
-    priority: 'High',
-    releaseDate: '11/03/2025',
-    assignedTo: 'Ebtesam, Deborah',
-    projectName: 'Time App',
-  });
+import { allTasks } from '../../../server/data/tasks.js';
 
 export const handlers = [
   http.get('/api/tasks', () => {
